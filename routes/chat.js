09e1fe74ac3be23c29c693e9c125dc101975d250
Carl Elbaz <carl.elbaz@gmail.com>
2015-02-04 14:17:07 -0500

    basic Webrtc Calling

--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('chat/index');
 });
 
 module.exports = router;
